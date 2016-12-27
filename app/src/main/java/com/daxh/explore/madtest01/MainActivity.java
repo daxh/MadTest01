19042c5cb4272dbb47eed484460a0a8623b8580c
Denis Suprun <suprundenis@gmail.com>
2016-12-27 10:18:05 +0300

    + Testing and configuring Pretty Logger usages

--- app/src/main/java/com/daxh/explore/madtest01/MainActivity.java
+++ app/src/main/java/com/daxh/explore/madtest01/MainActivity.java
@@ -3,11 +3,23 @@ package com.daxh.explore.madtest01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.daxh.explore.madtest01.Utils.LoggerUtils;
+import com.orhanobut.logger.Logger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoggerUtils.brief();
+        Logger.d("Pam Pam Point 1");
+
+        LoggerUtils.explicit();
+        Logger.d("Pam Pam Point 2");
+
+        LoggerUtils.brief();
+        Logger.d("Pam Pam Point 3");
     }
 }
